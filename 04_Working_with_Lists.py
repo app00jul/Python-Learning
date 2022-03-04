d765ae41b918b8675750f5d2320bc01a56cee71c
Julian Noeske <71953215+app00jul@users.noreply.github.com>
2022-03-04 16:45:34 +0000

    Update and rename 04. Working_with_Lists to 04_Working_with_Lists.py

--- 04_Working_with_Lists.py
+++ 04_Working_with_Lists.py
@@ -1,4 +1,4 @@
-4.1: Pizzas.
+#4.1: Pizzas.
 type_of_pizza = ["Hawaiian", "Seafood", "Greek"]
 
 for pizza in type_of_pizza:
@@ -6,7 +6,7 @@ for pizza in type_of_pizza:
 
 print("I really love pizza!")
 
-4.2: Animals.
+#4.2: Animals.
 pets = ["British short hair", "Golden Retriever", "Pug"]
 
 for pet in pets:
@@ -14,38 +14,38 @@ for pet in pets:
 
 print("All of them would make great pet!!!")
 
-4.3: Counting to Twenty.
+#4.3: Counting to Twenty.
 number = list(range(0,21))
 
 for num in number:
     print(num)
 
-4.4: One Million.
+#4.4: One Million.
 number = list(range(0,1000001))
 
 for num in number:
     print(num)
 
-4.5: Summing a Million.
+#4.5: Summing a Million.
 number = list(range(0,1000001))
 
 print(min(number))
 print(max(number))
 print(sum(number))
 
-4.6: Odd Numbers.
+#4.6: Odd Numbers.
 odd = list(range(1,21,2))
 
 for number in odd:
     print(number)
 
-4.7: Threes.
+#4.7: Threes.
 odd = list(range(3,31,3))
 
 for number in odd:
     print(number)
 
-4.8: Cubes.
+#4.8: Cubes.
 cubes = []
 
 for number in range(1,11):
@@ -55,11 +55,11 @@ for number in range(1,11):
 for cube_num in cubes:
     print(cube_num)
 
-4.9: Cube Comprehension.
+#4.9: Cube Comprehension.
 cubes = [cube**3 for cube in range(1,11)] 
 print(cubes)
 
-4.10: Slices.
+#4.10: Slices.
 cubes = [cube**3 for cube in range(1,11)]
 print(cubes)
 
@@ -74,7 +74,7 @@ while i < 3:
 for num in cubes[:3]:
     print(num)
 
-4.11: My Pizzas, Your Pizzas.
+#4.11: My Pizzas, Your Pizzas.
 my_pizzas = ["Hawaiian", "Seafood", "Greek"]
 friends_pizzas = my_pizzas[:]
 
@@ -88,7 +88,7 @@ friends_pizzas.append("peperoni")
 for friends_pizza in friends_pizzas:
     print(f"\t{friends_pizza}")
     
-4.12: More Loops.
+#4.12: More Loops.
 my_foods = ["pizza", "falafel", "carrot cake"]
 friend_foods = my_foods[:]
 
@@ -100,7 +100,7 @@ print("\nMy friend's favorite foods are: ")
 for friend_food in friend_foods:
     print(f"\t{friend_food}")
 
-4.13: Buffet.
+#4.13: Buffet.
 foods = ("Butter bread", "Dimsum", "Pho", "Tonkotsu", "Kimbap")
 
 print("The five foods that the restaurant serves are: ")
@@ -114,8 +114,8 @@ print("\nThe new list of foods that the restaurant serves are: ")
 for food in foods:
     print(f"\t{food}")
 
-4.14: PEP 8.
-Finished!
+#4.14: PEP 8.
+#Finished!
 
-4.15: Code Review.
-Do it yourself!
+#4.15: Code Review.
+#Do it yourself!
