8ec51aabb4a7fdc9d77bcfd51b61a530da841a1f
Julian Noeske <71953215+app00jul@users.noreply.github.com>
2022-03-04 16:42:49 +0000

    Update and rename 02. Variables_and_Simple_Data_Types to 02. Variables_and_Simple_Data_Types.py

--- 02. Variables_and_Simple_Data_Types.py
+++ 02. Variables_and_Simple_Data_Types.py
@@ -1,11 +1,11 @@
-2.1: Simple Message.
+#2.1: Simple Message.
 
 message = "God please saves me!"
 
 print(message)
 
 
-2.2: Simple Message.
+#2.2: Simple Message.
 
 message = "God please saves me!"
 
@@ -16,14 +16,14 @@ message = "1 Corinthians 13:4-8"
 print(message)
 
 
-2.3: Personal Message.
+#2.3: Personal Message.
 
 Jesus = "Hi Christ, thank you for your salvation"
 
 print(Jesus)
 
 
-2.4: Name Cases.
+#2.4: Name Cases.
 name = "Elon Musk"
 
 print(name.upper())
@@ -31,7 +31,7 @@ print(name.lower())
 print(name.title())
 
 
-2.5, 2.6: Famous Quote 1 + 2.
+#2.5, 2.6: Famous Quote 1 + 2.
 quote = "Take courage! It is I. Don’t be afraid"
 
 speaker = "Jesus"
@@ -39,7 +39,7 @@ speaker = "Jesus"
 print(f"{speaker} once said '{quote}'")
 
 
-2.7: Stripping name.
+#2.7: Stripping name.
 name = " Wub Wub "
 
 print(name.rstrip())
@@ -48,7 +48,7 @@ print(name.lstrip())
 
 print(name.strip())
 
-2.8: Number Eight.
+#2.8: Number Eight.
 print(2+6)
 
 print(9-1)
@@ -58,18 +58,17 @@ print(2*4)
 print(32/4)
 
 
-2.9: Favorite number.
+#2.9: Favorite number.
 fav_number = "7"
 
 fav_number = str(fav_number)
 
 print(f"My favorite number is {fav_number}")
 
-2.10: Adding Comments.
-Ewwww no xD.
+#2.10: Adding Comments.
 
 
-2.11: Zen of Python.
+#2.11: Zen of Python.
 import this
 
 
